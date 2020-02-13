0367dec7d94562d94da01f6972fcff98b1b51ddd
hchang23 <hchang0223@gmail.com>
2020-02-13 15:50:45 -0800

    Add chapter 16 exercises

--- chapter-16-exercises/exercise-2/exercise.R
+++ chapter-16-exercises/exercise-2/exercise.R
@@ -12,52 +12,68 @@ library("ggplot2")
 
 # Draw a column (bar) chart of diamonds cuts by price, with each bar filled by 
 # clarity. You should see a _stacked_ bar chart.
-
+ggplot(diamonds, aes(cut, price, fill = clarity)) +
+  geom_col()
 
 # Draw the same chart again, but with each element positioned to "fill" the y axis
-
+ggplot(diamonds, aes(cut, price, fill = clarity)) +
+  geom_col(position = "fill")
 
 # Draw the same chart again, but with each element positioned to "dodge" each other
-
+ggplot(diamonds, aes(cut, price, fill = clarity)) +
+  geom_col(position = "dodge")
 
 # Draw a plot with point geometry with the x-position mapped to `cut` and the 
 # y-position mapped to `clarity`
 # This creates a "grid" grouping the points
-
+ggplot(diamonds, aes(cut, clarity)) +
+  geom_point()
 
 # Use the "jitter" position adjustment to keep the points from all overlapping!
 # (This works a little better with a sample of diamond data, such as from the 
 # previous exercise).
-
+ggplot(diamond_sample, aes(cut, clarity)) +
+  geom_point(position = "jitter")
 
 
 ## Scales
 
 # Draw a "boxplot" (with `geom_boxplot`) for the diamond's price (y) by color (x)
-
+ggplot(diamonds, aes(color, price)) +
+  geom_boxplot()
 
 # This has a lot of outliers, making it harder to read. To fix this, draw the 
 # same plot but with a _logarithmic_ scale for the y axis.
-
+ggplot(diamonds, aes(color, price)) +
+  geom_boxplot() +
+  scale_y_log10()
 
 # For another version, draw the same plot but with `violin` geometry instead of 
 # `boxplot` geometry!
 # How does the logarithmic scale change the data presentation?
-
+ggplot(diamonds, aes(color, price)) +
+  geom_violin() +
+  scale_y_log10()
 
 # Another interesting plot: draw a plot of the diamonds price (y) by carat (x), 
 # using a heatmap of 2d bins (geom_bin2d)
 # What happens when you make the x and y channels scale logarithmically?
-
+ggplot(diamonds, aes(carat, price)) +
+  geom_bin2d() +
+  scale_x_log10() +
+  scale_y_log10()
 
 # Draw a scatter plot for the diamonds price (y) by carat (x). Color each point
 # by the clarity (Remember, this will take a while. Use a sample of the diamonds 
 # for faster results)
-
+ggplot(diamond_sample, aes(carat, price, color = clarity)) +
+  geom_point()
 
 # Change the color of the previous plot using a ColorBrewer scale of your choice. 
 # What looks nice?
-
+ggplot(diamond_sample, aes(carat, price, color = clarity)) +
+  geom_point() +
+  scale_color_brewer(palette = "Set2")
 
 
 ## Coordinate Systems
@@ -66,20 +82,26 @@ library("ggplot2")
 # For best results, SET the `width` of the geometry to be 1 (fill plot, no space
 # between)
 # TIP: You can save the plot to a variable for easier modifications
-
+ggplot(diamond_sample, aes(cut, fill = cut)) +
+  geom_bar(width = 1)
 
 # Draw the same chart, but with the coordinate system flipped
-
+ggplot(diamond_sample, aes(cut, fill = cut)) +
+  geom_bar(width = 1) +
+  coord_flip()
 
 # Draw the same chart, but in a polar coordinate system. It's a Coxcomb chart!
-
-
+ggplot(diamond_sample, aes(cut, fill = cut)) +
+  geom_bar(width = 1) +
+  coord_polar()
 
 ## Facets
 
 # Take the scatter plot of price by carat data (colored by clarity) and add 
 # _facets_ based on the diamond's `color`
-
+ggplot(diamond_sample, aes(carat, price, color = clarity)) +
+  geom_point() +
+  facet_wrap(~color)
 
 
 ## Saving Plots
@@ -87,4 +109,4 @@ library("ggplot2")
 # Use the `ggsave()` function to save the current (recent) plot to disk.
 # Name the output file "my-plot.png".
 # Make sure you've set the working directory!!
-
+ggsave("./my-plot.png")
